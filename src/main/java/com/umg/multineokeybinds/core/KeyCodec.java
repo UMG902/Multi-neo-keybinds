570317f84148da22736d9936dc1e16fb125a2563
UMG902 <otmanemed09@gmail.com>
2026-06-13 16:32:07 +0100

    Delete src/main directory

--- src/main/java/com/umg/multineokeybinds/core/KeyCodec.java
+++
@@ -1,8 +0,0 @@
-package com.umg.multineokeybinds.core;
-
-public class KeyCodec {
-
-    public static boolean isPressed(long window, int key) {
-        return org.lwjgl.glfw.GLFW.glfwGetKey(window, key) == 1;
-    }
-}
