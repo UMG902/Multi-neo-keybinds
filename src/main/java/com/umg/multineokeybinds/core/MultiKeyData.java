22478b1a6a95ac90c20d4237df09e8b0d033f367
UMG902 <otmanemed09@gmail.com>
2026-06-13 18:31:10 +0100

    Delete src/main/java/com/umg directory

--- src/main/java/com/umg/multineokeybinds/core/MultiKeyData.java
+++
@@ -1,8 +0,0 @@
-package com.umg.multineokeybinds.core;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MultiKeyData {
-    public final List<Integer> extraKeys = new ArrayList<>();
-}
