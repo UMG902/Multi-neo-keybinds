da3e2af1890f4fb6cf6d8977ff0f421f2e451095
UMG902 <otmanemed09@gmail.com>
2026-06-13 17:46:07 +0100

    Delete src/main directory

--- src/main/java/com/umg/multineokeybinds/client/KeyEntryExtension.java
+++
@@ -1,6 +0,0 @@
-package com.umg.multineokeybinds.client;
-
-public interface KeyEntryExtension {
-    boolean multikeybinds$handleClick(double mouseX, double mouseY, int button);
-    boolean multikeybinds$handleRelease(double mouseX, double mouseY, int button);
-}
