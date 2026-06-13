3f76c74db0789897accf3ca4c31f71cb74abbab2
UMG902 <otmanemed09@gmail.com>
2026-06-13 15:29:14 +0100

    Add files via upload

---
+++ src/main/java/com/umg/multineokeybinds/KeyBindsScreenExtension.java
@@ -0,0 +1,8 @@
+package com.umg.multineokeybinds.mixin;
+
+import net.minecraft.client.gui.screens.options.controls.KeyBindsList;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(KeyBindsList.class)
+public abstract class KeyBindsListMixin {
+}
