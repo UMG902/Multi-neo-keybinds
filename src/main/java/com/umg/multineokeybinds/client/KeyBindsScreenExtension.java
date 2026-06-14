6fa7962fc63a53d22b55c3c1861bc39cf94e6b3e
UMG902 <otmanemed09@gmail.com>
2026-06-14 16:05:55 +0100

    Delete src directory

--- src/main/java/com/umg/multineokeybinds/client/KeyBindsScreenExtension.java
+++
@@ -1,8 +0,0 @@
-package com.umg.multineokeybinds.client;
-
-import net.minecraft.client.KeyMapping;
-
-public interface KeyBindsScreenExtension {
-    void multikeybinds$startListening(KeyMapping mapping);
-    void multikeybinds$startListening(KeyMapping mapping, Integer keyToReplace);
-}
